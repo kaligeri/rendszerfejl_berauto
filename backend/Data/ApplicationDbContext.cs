a74a2bb28db1a648bbc652f6b3957d899266353f
u_zsolt <bozsika2004@gmail.com>
2026-03-03 11:18:48 +0100

    DTOs, Data

---
+++ backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BerAuto.Backend.Entities;
+
+namespace backend.Data
+{
+    /*
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+    */
+}
