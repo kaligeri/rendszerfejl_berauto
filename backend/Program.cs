407a1705e06e40bce9d89c3b36708c560432bb5e
Borsos Zalán <muonlinezali@gmail.com>
2026-04-22 15:41:12 +0200

    Rent logic

--- backend/Program.cs
+++ backend/Program.cs
@@ -29,7 +29,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddScoped<ICarService, CarService>();
 builder.Services.AddScoped<IPasswordHasher, PasswordHasher>(); 
-builder.Services.AddScoped<IAuthService, AuthService>();      
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IRentalService, RentalService>();
 
 
 var jwtSettings = builder.Configuration.GetSection("AppSettings:Token").Value;
---
+++ backend/Services/IRentalService.cs
@@ -0,0 +1,16 @@
+﻿using BerAuto.Backend.Entities;
+
+namespace BerAuto.Backend.Services;
+
+public interface IRentalService
+{
+
+    Task<IEnumerable<Rental>> GetAllRentalsAsync();
+
+
+    Task<IEnumerable<Rental>> GetUserRentalsAsync(Guid userId);
+
+    Task<Rental?> CreateRentalAsync(Rental rental);
+
+    Task<bool> UpdateStatusAsync(int rentalId, RentalStatus newStatus);
+}
